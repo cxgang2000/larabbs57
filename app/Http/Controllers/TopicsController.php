9c63a9ef0f2f2724bf030f77e6e01f4ef78c4c91
vagrant <vagrant@homestead>
2019-03-18 04:21:28 +0000

    侧边栏链接

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -12,6 +12,7 @@ use Auth;
 use App\Handlers\ImageUploadHandler;
 
 use App\Models\User;
+use App\Models\Link;
 
 class TopicsController extends Controller
 {
@@ -26,12 +27,13 @@ class TopicsController extends Controller
     //  $topics = Topic::with('user', 'category')->paginate(30);
 	// 	return view('topics.index', compact('topics'));
 	// }
-    public function index(Request $request, Topic $topic, User $user)
+    public function index(Request $request, Topic $topic, User $user, Link $link)
     {
         $topics = $topic->withOrder($request->order)->paginate(20);
         $active_users = $user->getActiveUsers();
-        // dd($active_users);
-        return view('topics.index', compact('topics', 'active_users'));
+        $links = $link->getAllCached();
+
+        return view('topics.index', compact('topics', 'active_users', 'links'));
     }
 
     public function show(Request $request, Topic $topic)
